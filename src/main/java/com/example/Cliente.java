858f30937c454a32a70d5a1ed3f9f7f5adef29dd
Torr1co <fabriziotorrico27@gmail.com>
2023-05-24 15:05:29 -0300

    Encapsulate Fields - Client

--- src/main/java/com/example/Cliente.java
+++ src/main/java/com/example/Cliente.java
@@ -4,51 +4,68 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cliente {
-	public String nombreYApellido;
-public String tipo;
-public String telefono;
-public String cuit;
-public String dni;
-public List<Llamada>llamadasRealizadas = new ArrayList<Llamada>(); ;
-public Persoonal sistema;
-	
-	public List<Llamada> getllamadasRealizadas() {
-		return llamadasRealizadas;
-	}
-	public void setllamadasRealizadas(List<Llamada> llamadasRealizadas) {
-		this.llamadasRealizadas = llamadasRealizadas;
-	}
-	public String getTipo() {
-		return tipo;
-	}
-	public void setT(String tipo) {
-		this.tipo = tipo;
-	}
-	public String getNombreYApellido() {
-		return nombreYApellido;
-	}
-	public void setnombreYApellido(String nombreYApellido) {
-		this.nombreYApellido = nombreYApellido;
-	}
-	public String getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-	public String getCuit() {
-		return cuit;
-	}
-	public void setCuit(String cuit) {
-		this.cuit = cuit;
-	}
-	public String getDni() {
-		return dni;
-	}
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-	
+	private String nombreYApellido;
+private String tipo;
+private String telefono;
+private String cuit;
+private String dni;
+private List<Llamada>llamadasRealizadas = new ArrayList<Llamada>(); ;
+private Persoonal sistema;
 
-	
+public String getNombreYApellido() {
+    return nombreYApellido;
+}
+
+public void setNombreYApellido(String nombreYApellido) {
+    this.nombreYApellido = nombreYApellido;
+}
+
+public String getTipo() {
+    return tipo;
+}
+
+public void setTipo(String tipo) {
+    this.tipo = tipo;
+}
+
+public String getTelefono() {
+    return telefono;
+}
+
+public void setTelefono(String telefono) {
+    this.telefono = telefono;
+}
+
+public String getCuit() {
+    return cuit;
+}
+
+public void setCuit(String cuit) {
+    this.cuit = cuit;
+}
+
+public String getDni() {
+    return dni;
+}
+
+public void setDni(String dni) {
+    this.dni = dni;
+}
+
+public ArrayList<Llamada> getLlamadasRealizadas() {
+    return llamadasRealizadas;
+}
+
+public void setLlamadasRealizadas(ArrayList<Llamada> llamadasRealizadas) {
+    this.llamadasRealizadas = llamadasRealizadas;
+}
+
+public Persoonal getSistema() {
+    return sistema;
+}
+
+public void setSistema(Persoonal sistema) {
+    this.sistema = sistema;
+}
+
 }
